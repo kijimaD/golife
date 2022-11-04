cf5b17fc059c9392b0a943143dd6784c4e9ee677
kijimaD <norimaking777@gmail.com>
2022-11-04 14:43:41 +0900

    fix: トーラスを正しく処理できてなかったのを修正した

--- util/util.go
+++ util/util.go
@@ -1,8 +1,22 @@
 package util
 
+// 2次元配列で相対移動したインデックスを求める
+// w: 2次元配列の横と縦の長さ。今のところ正方形しか対応してない
+// base: 基準になるインデックス
+// rx: xの相対的な位置
+// ry: yの相対的な位置
+func PlaneIndex(w int, base int, rx int, ry int) int {
+	x := CalcIndex(w, base, rx)
+	y := CalcIndex(w, base, ry)
+
+	return x + y*w
+}
+
+// 1次元配列で相対移動したインデックスを求める
+// w: 実配列の長さ
+// x: 基準になるインデックス
+// r: 相対的な位置
 func CalcIndex(w int, x int, r int) int {
-	// x: 基準になるインデックス
-	// r: 相対的な位置
 	nx := (x + r) % w
 	if nx < 0 {
 		nx += w
--- util/util_test.go
+++ util/util_test.go
@@ -4,14 +4,79 @@ import (
 	"testing"
 )
 
-func TestCalcIndex(t *testing.T) {
+func TestPlaneIndex(t *testing.T) {
 	// 0 1 2
 	// 3 4 5
 	// 6 7 8
 
 	// 4からの相対位置指定で、8方向分のインデックスを取得するテスト
-	len := 9
 	cur := 4
+	col := 3
+
+	tests := []struct {
+		iw       int
+		base     int
+		rx       int
+		ry       int
+		expected int
+	}{
+		{col, cur, 0, 0, 4},   // 元の位置
+		{col, cur, 0, -1, 1},  // 上
+		{col, cur, 1, -1, 2},  // 右上
+		{col, cur, 1, 0, 5},   // 右
+		{col, cur, 1, 1, 8},   // 右下
+		{col, cur, 0, 1, 7},   // 下
+		{col, cur, -1, 1, 6},  // 左下
+		{col, cur, -1, 0, 3},  // 左
+		{col, cur, -1, -1, 0}, // 左上
+	}
+
+	for i, tt := range tests {
+		if PlaneIndex(tt.iw, tt.base, tt.rx, tt.ry) != tt.expected {
+			t.Errorf("idx: %d is not match", i)
+		}
+	}
+}
+
+func TestPlaneIndexEdge(t *testing.T) {
+	// 0 1 2
+	// 3 4 5
+	// 6 7 8
+
+	// 0からの相対位置指定で、8方向分のインデックスを取得するテスト。トーラス処理を確かめる
+	cur := 0
+	col := 3
+
+	tests := []struct {
+		iw       int
+		base     int
+		rx       int
+		ry       int
+		expected int
+	}{
+		{col, cur, 0, 0, 0},   // 元の位置
+		{col, cur, 0, -1, 6},  // 上
+		{col, cur, 1, -1, 7},  // 右上
+		{col, cur, 1, 0, 1},   // 右
+		{col, cur, 1, 1, 4},   // 右下
+		{col, cur, 0, 1, 3},   // 下
+		{col, cur, -1, 1, 5},  // 左下
+		{col, cur, -1, 0, 2},  // 左
+		{col, cur, -1, -1, 8}, // 左上
+	}
+
+	for i, tt := range tests {
+		if PlaneIndex(tt.iw, tt.base, tt.rx, tt.ry) != tt.expected {
+			t.Errorf("idx: %d is not match", i)
+		}
+	}
+}
+
+func TestCalcIndex(t *testing.T) {
+	// 0 1 2
+
+	// 0からの相対位置指定で、2方向分のインデックスを取得するテスト
+	cur := 0
 	col := 3
 
 	tests := []struct {
@@ -20,56 +85,14 @@ func TestCalcIndex(t *testing.T) {
 		ip       int
 		expected int
 	}{
-		{len, cur, 0, 4},        // 元の位置
-		{len, cur, -col, 1},     // 上
-		{len, cur, -col + 1, 2}, // 右上
-		{len, cur, 1, 5},        // 右
-		{len, cur, col + 1, 8},  // 右下
-		{len, cur, col, 7},      // 下
-		{len, cur, col - 1, 6},  // 左下
-		{len, cur, -1, 3},       // 左
-		{len, cur, -col - 1, 0}, // 左上
+		{col, cur, 0, 0},  // 元の位置
+		{col, cur, 1, 1},  // 右
+		{col, cur, -1, 2}, // 左
 	}
 
 	for i, tt := range tests {
-		if calcIndex(tt.iw, tt.ix, tt.ip) != tt.expected {
-			t.Errorf("idx: %d is not match", i)
-		}
-	}
-}
-
-func TestCalcIndex2(t *testing.T) {
-	// 0  1  2  3  4
-	// 5  6  7  8  9
-	// 10 11 12 13 14
-	// 15 16 17 18 19
-	// 20 21 22 23 24
-
-	// 4からの相対位置指定で、8方向分のインデックスを取得するテスト
-	len := 25
-	cur := 12
-	col := 5
-
-	tests := []struct {
-		iw       int
-		ix       int
-		ip       int
-		expected int
-	}{
-		{len, cur, 0, 12},       // 元の位置
-		{len, cur, -col, 7},     // 上
-		{len, cur, -col + 1, 8}, // 右上
-		{len, cur, 1, 13},       // 右
-		{len, cur, col + 1, 18}, // 右下
-		{len, cur, col, 17},     // 下
-		{len, cur, col - 1, 16}, // 左下
-		{len, cur, -1, 11},      // 左
-		{len, cur, -col - 1, 6}, // 左上
-	}
-
-	for i, tt := range tests {
-		if calcIndex(tt.iw, tt.ix, tt.ip) != tt.expected {
-			t.Errorf("idx: %d is not match", i)
+		if r := CalcIndex(tt.iw, tt.ix, tt.ip); r != tt.expected {
+			t.Errorf("idx: %d is not match. result: %d, expected: %d", i, r, tt.expected)
 		}
 	}
 }
